2ff9ff1e04a3259c6a9ec04594c4445f9df75e24
djmoore926 <djmoore926@gmail.com>
2024-12-16 22:32:18 -0500

    blank gui window working

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        color = pygame.Color("black")
+        screen.fill(color, rect = None, special_flags = 0)
+        pygame.display.flip()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
